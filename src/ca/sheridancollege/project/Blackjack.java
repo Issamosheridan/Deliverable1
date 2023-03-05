b52b5765d816b5a2ed6f1fd8370cf11d5fdc40dd
Mohammed Issa <issamo@sheridancollege.ca>
2023-03-04 21:12:46 -0500

    group project

---
+++ src/ca/sheridancollege/project/Blackjack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author moeissa
+ */
+public class Blackjack {
+    public static void main void(String [] args){
+
+}
+    
+}
